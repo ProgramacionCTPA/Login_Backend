983ad82101bb54b5b46bbfa43ab1a6546b96da2a
ProgramacionCTPA <160040652+ProgramacionCTPA@users.noreply.github.com>
2026-03-01 20:14:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.post("/login", async (req, res) => {
    }
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
 
